3f0796ab0ea25d73f883b57e0a7f45b3faf38224
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2023-12-20 00:03:07 +0530

    updated cdn

--- Server.js
+++ Server.js
@@ -1,31 +1,39 @@
 const fs = require('fs')
 const https = require('https')
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 const db = require('./MongoConnect')
 const helmet = require('helmet')
 const { signup, login, getUser } = require('./Controllers/UserController')
-const { addDoctor } = require('./Controllers/DoctorController')
-const { addClinic, displayClinics, displayBySpecialists, displayByLocality } = require('./Controllers/ClinicController')
 const UserModel = require('./Models/UserModel')
-const auth = require('./Middleware/AuthMW')
-
-const multer = require('multer');
-const upload = multer({ dest: null });
-
+const DoctorModel = require('./Models/DoctorModel')
+const ClinicModel = require('./Models/ClinicModel')
 const sgMail = require('@sendgrid/mail');
+const auth = require('./Middleware/AuthMW')
+const { profile } = require('console')
+const { uploadProfilePic } = require('./Middleware/UploadProfilePic')
+const multer = require('multer');
 
-//Dummy Commit
 
 const app = express()
+
+const storage = multer.memoryStorage(); // You can also use diskStorage if you want to save files to disk
+const upload = multer({ storage: storage });
+
+app.use(express.urlencoded({ extended: true })); // Parse application/x-www-form-urlencoded requests
+app.use(upload.any()); // Use multer to handle multipart/form-data
+
+
+// ******* MIDDLEWARES ********
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
 
 const PORT = process.env.PORT || 9000
 
-app.use(helmet())
+
+// ************ API ENDPOINTS ************ 
 
 app.get('/', (req, res) => {
     res.send({ "msg": "Connection Successful!" })
@@ -105,15 +113,137 @@ app.post("/login/google", async (req, res) => {
 });
 
 
-app.post("/add-doctor", addDoctor);
+app.post("/add-doctor", async (req, res) => {
 
-app.post('/add-clinic', addClinic);
+    console.log("Request body:", req.body);
+    const { name, qualification, yearsOfExperience, description, phoneNumber } = req.body;
 
-app.get('/clinics', displayClinics);
+    if (!name || !qualification || !yearsOfExperience || !description) {
+        return res.status(400).json({ error: "Please fill in all required fields." });
+    }
 
-app.get('/clinics/specialists', displayBySpecialists);
+    const existingDoctor = await DoctorModel.findOne({ phoneNumber });
+    if (existingDoctor) {
+        return res.status(400).json({ error: "Doctor already exists." });
+    }
 
-app.get('/clinics/locality', displayByLocality);
+    var secureUrl = ""
+
+    if(req.body.profilePic){
+        secureUrl = await uploadProfilePic(req.body.profilePic)
+    }
+
+    const newDoctor = new DoctorModel({
+        profilePic: secureUrl,
+        name: name,
+        qualification: qualification,
+        yearsOfExperience: yearsOfExperience,
+        description: description,
+        phoneNumber: phoneNumber
+    });
+
+    try {
+        await newDoctor.save();
+        console.log("New doctor added:", newDoctor);
+        return res.status(200).json({ message: "Doctor added successfully." });
+    } catch (error) {
+            console.error("Error adding doctor:", error);
+            return res.status(500).json({ error: "An error occurred. Please try again later." });
+    }
+});
+
+
+const isValidUrl = url => {
+    try {
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+const isValidTime = time => {
+    const regex = /^([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
+    return regex.test(time);
+};
+
+app.post('/add-clinic', async (req, res) => {
+    try {
+        const { clinicName, address, directionUrl, specialists, phoneNumber, doctors } = req.body;
+
+        const existingClinic = await ClinicModel.findOne({ phoneNumber });
+
+        if (existingClinic) {
+            return res.status(409).send({ message: 'Clinic with the phone number already exists.' });
+        }
+
+        if (!clinicName || typeof clinicName !== 'string') {
+            return res.status(400).send({ message: 'Clinic name is required and must be a string.' });
+        }
+
+        if (typeof directionUrl !== 'string' || !isValidUrl(directionUrl)) {
+            return res.status(400).send({ message: 'Direction URL is required and must be a valid URL.' });
+        }
+
+        if (!specialists || !Array.isArray(specialists) || !specialists.every(specialist => typeof specialist === 'string')) {
+            return res.status(400).send({ message: 'Specialists must be an array of strings.' });
+        }
+
+        if (!doctors || !Array.isArray(doctors) || !doctors.every(doctor => typeof doctor === 'object')) {
+            return res.status(400).send({ message: 'Doctors must be an array of objects.' });
+        }
+
+        for (const doctor of doctors) {
+            const { _id, daysAvailable, timings, fees } = doctor;
+
+            if (!mongoose.Types.ObjectId.isValid(_id)) {
+                return res.status(400).send({ message: 'Doctor ID is invalid.' });
+            }
+
+            if (!daysAvailable || !Array.isArray(daysAvailable) || !daysAvailable.every(day => typeof day === 'string')) {
+                return res.status(400).send({ message: 'Days available must be an array of strings.' });
+            }
+
+            for (const timing of timings) {
+                const { day, startTime, endTime } = timing;
+
+                if (!day || typeof day !== 'string') {
+                    return res.status(400).send({ message: 'Day within timing must be a string.' });
+                }
+
+                if (!startTime || !isValidTime(startTime)) {
+                    return res.status(400).send({ message: 'Start time within timing is invalid.' });
+                }
+
+                if (!endTime || !isValidTime(endTime)) {
+                    return res.status(400).send({ message: 'End time within timing is invalid.' });
+                }
+            }
+
+            if (!fees || typeof fees !== 'number' || isNaN(fees)) {
+                return res.status(400).send({ message: 'Doctor fees must be a valid number.' });
+            }
+        }
+
+        const clinic = new ClinicModel({
+            clinicName,
+            address,
+            directionUrl,
+            specialists,
+            phoneNumber,
+            doctors
+        });
+
+        console.log(clinic);
+
+        await clinic.save();
+
+        res.status(201).send({ message: 'Clinic added successfully!', clinic });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'An internal server error occurred.' });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
